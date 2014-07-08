394842a08bea39f9908a919642250ff38357925e
nicolaslarsen <nicolasringsmose@hotmail.com>
2014-07-08 23:27:01 +0200

    Rettede tidligere fejl, som ikke var opdateret i vores git

--- Skoledatabase.sql
+++ Skoledatabase.sql
@@ -16,11 +16,11 @@ CREATE TABLE Users(
     salt CHAR(128) NOT NULL,
     PRIMARY KEY (email));
     
-INSERT INTO Users VALUES('123@hotmail.com','c275a088774aa9684896e4a9be0ea5e3','Anders','Balling',13371337,'n', 123123132);
+INSERT INTO Users VALUES('123@hotmail.com','4e90b2bee1e1e1fcec26c62415ab43cb','Anders','Balling',13371337,'n', 123123132);
 INSERT INTO Users VALUES('Eriklarsen@lel.dk','e10adc3949ba59abbe56e057f20f883e','Erik','Larsen',31453415,'n', 313123213);
 INSERT INTO Users VALUES('lelele@wup.dk','7b217fc6f2690762d0ed3c4f7324a495','Hundrede P','Bror',12376512,'n', 131232321323);
 INSERT INTO Users VALUES('nummerto@wup.dk','d38a132a07fe5dfe0e83272a2250f2cb','Jeg er','toeren',22222222,'n', 321321321321); 
-INSERT INTO Users VALUES('Admin@lel.dk','89e38e6cb0a24671ff72148c28a7a175','Admin', NULL, NULL, 'y', 321321321321);
+INSERT INTO Users VALUES('Admin@lel.dk','f02291917873266e36a6dfe2ff5bdd1b','Admin', NULL, NULL, 'y', 321321321321);
 INSERT INTO Users VALUES('test@123.dk', 'e10adc3949ba59abbe56e057f20f883e', 'Børge', 'Bæversen', 12312345, 'n', 12312321321333); 
     
 CREATE TABLE Children(
@@ -53,6 +53,6 @@ INSERT INTO Has Values('nummerto@wup.dk',8077380773);
 INSERT INTO Has Values('123@hotmail.com',1812941738);
 
 CREATE TABLE Messages(
-    message TEXT);
+    message VARCHAR(4000));
     
 commit;
